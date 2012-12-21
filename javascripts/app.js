713bb5d6aaa1e06e2f79b788f3683af0369a6f1b
Bruno Henrique Stein <brunohstein@gmail.com>
2012-12-21 15:27:13 -0200

    aargh

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-  $.getJSON('http://api.dribbble.com/players/brunohstein/shots?callback=?', 
-    {},
-    function (data) {
-      $.each(data.shots, function(i, shot) {
-        $('.work').append([
-          '<div class="shot">',
-            '<a href="' + shot.url + '" target="_blank">',
-              '<img src="' + shot.image_url + '" />',
-            '</a>',
-          '</div>'
-        ].join(''));
-      });  
-    }
-  );
-
-  
-  function setAboutHeight() {
-    var newHeight = $('.about').height() + 95;
-
-    $('.clear').css('height', newHeight);
-  };
-
-  setAboutHeight();
-
-  $(window).resize(function() {
-    setAboutHeight();
-  });
+  // nothing to do here!
 
 });
